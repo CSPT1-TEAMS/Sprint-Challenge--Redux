f43d527e5ad5b2c7c0adabee0eaafc27f9698f73
ubuntu <irishseanvaldivia@gmail.com>
2018-04-30 13:23:51 -0500

    Finished action types.

---
+++ smurfs/src/actions/types.js
@@ -0,0 +1,14 @@
+export const FETCHING = 'FETCHING';
+export const CREATING = 'CREATING';
+export const UPDATING = 'UPDATING';
+export const DELETING = 'DELETING';
+export const SUCCESS = 'SUCCESS';
+export const ERROR = 'ERROR';
+export const EDITING = 'EDITING';
+export const CANCEL_EDIT = 'CANCEL_EDIT';
+export const INPUT_CHANGE = 'INPUT_CHANGE';
+
+// so we make our action types in a separate file
+// it's really nothing more than a simple ol js file
+// with a bunch of string variables inside of it.
+// and we export those strings the moment they are created.
