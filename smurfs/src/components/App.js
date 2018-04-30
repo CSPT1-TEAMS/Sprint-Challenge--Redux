0e7690d81f811c6c513c37a15cb671d551e0fb2e
ubuntu <irishseanvaldivia@gmail.com>
2018-04-30 17:00:54 -0500

    finished App.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Form from './Form';
+import Smurf from './Smurf';
+import { getSmurfs } from '../actions'
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I connect my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+
 class App extends Component {
+  componentDidMount = () => {
+    this.props.getSmurfs()
+  }
+  
   render() {
+    const { smurfs, pending, error } = this.props
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
+        <div>Welcome to the Redux version of Smurfs!!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        {pending ? 'Communicating with the server...' : null}
+        {error ? `Whoopsy! You done smurfed up! ${error.toString()}` : null}
+        <Form />
+        {!smurfs
+          ? 'Loading...'
+          : smurfs.map(smurf => <Smurf key={smurf.id} smurf={smurf}/>)
+        }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const mapStateToProps = state => console.log('mapping da state:', state) || ({
+  smurfs: state.smurfs,
+  pending: state.pending,
+  error: state.error,
+})
+
+const mapDispatchToProps = {
+  getSmurfs,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App) // okay so explain this line of code 
+// why the (app) after the connect
+// help me understand the syntax and why
